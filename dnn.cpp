be4cceaaef59ca2e130a2177bff2edd28e829e93
Venci Freeman <37969638+VenciFreeman@users.noreply.github.com>
2019-11-15 07:55:57 +0800

    Add some thinking.

--- dnn.cpp
+++ dnn.cpp
@@ -11,6 +11,14 @@ int dnn(
 	int label = 0; //label is the predicted result by dnn
 	//==================== Insert your code here ====================//
 	
+		// input_image[IMAGE_SIZE]*w1[IMAGE_SIZE][HIDDEN_LAYER]
+		// +b1[HIDDEN_LAYER]
+		// hidden[HIDDEN_LAYER] = ReLU(x)	// ReLU(x) = max(0,x)
+		// hidden[HIDDEN_LAYER]*w2[HIDDEN_LAYER][OUTPUT_CLASS]
+		// +b2[OUTPUT_CLASS]
+		// output[OUTPUT_CLASS]
+		// predict
+
 	//===============================================================//
 	return label;
 }
