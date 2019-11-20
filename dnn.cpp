0ddfb119d9286a67bf339219d3e78e567c9f1abe
Venci Freeman <37969638+VenciFreeman@users.noreply.github.com>
2019-11-20 22:13:02 +0800

    Edit details.

--- dnn.cpp
+++ dnn.cpp
@@ -1,5 +1,6 @@
 #include "dnn.h"
 //#define ReLU(a, b) (a > b ? a : b)
+// If i use marco define, there will be some error about fixed point.
 
 int dnn(
         image_t input_image[IMAGE_SIZE],
@@ -10,38 +11,44 @@ int dnn(
 )
 {
     int label = 0; //label is the predicted result by dnn
-    int i,j;
+    int i,j;    // Loop variable
     FIXED temp = 0;
     FIXED hidden[HIDDEN_LAYER];
-    LOOP2H:for (i=0; i<HIDDEN_LAYER; i++){
+    FIXED output[OUTPUT_CLASS];
+
+    // I tried to use b1[],b2[] instead of intermediate arrays, but there would be something wrong.
+
+    LOOP1H:for (i=0; i<HIDDEN_LAYER; i++) {
     	hidden[i]=0;
     }
-    FIXED output[OUTPUT_CLASS];
-    LOOP1H:for (i=0; i<OUTPUT_CLASS; i++){
+    
+    LOOP2H:for (i=0; i<OUTPUT_CLASS; i++) {
     	output[i]=0;
     }
     //memset(hidden,0,HIDDEN_LAYER * sizeof(hidden[0]));
     //memset(output,0,OUTPUT_CLASS * sizeof(output[0]));
 
+    // I use the original method to initialize because if I use memset() or just equals the array to {0}, the will be something wrong about accuracy.
+
     LOOP1I:for (i=0; i<IMAGE_SIZE; i++) {  // input_image[IMAGE_SIZE]*w1[IMAGE_SIZE][HIDDEN_LAYER]
-    LOOP1J:for (j=0; j<HIDDEN_LAYER; j++) {
-    hidden[j] += input_image[i] * w1[i][j];
-    }
+        LOOP1J:for (j=0; j<HIDDEN_LAYER; j++) {
+        hidden[j] += input_image[i] * w1[i][j];
+        }
     // hidden[i] = ReLU(temp,(hidden[i] + b1[i]));  // +b1[HIDDEN_LAYER] and ReLU(hidden[HIDDEN_LAYER]) = max(0,hidden[HIDDEN_LAYER])
-}
+    }
 
     LOOP1K:for (i=0; i<HIDDEN_LAYER; i++) {
-    	hidden[i] = hidden[i] + b1[i];
-        if (hidden[i] > 0)
-            hidden[i] = temp;
-    }
+    	hidden[i] += b1[i];
+          if (hidden[i]  < 0)
+        	  hidden[i] = temp;
+    }// ReLU. More loops is better for HLS.
 
     LOOP2I:for (i=0; i<HIDDEN_LAYER; i++) {  // hidden[HIDDEN_LAYER]*w2[HIDDEN_LAYER][OUTPUT_CLASS]
-    LOOP2J:for (j=0; j<OUTPUT_CLASS; j++) {
-    output[j] += hidden[i] * w2[i][j];
-    }
+        LOOP2J:for (j=0; j<OUTPUT_CLASS; j++) {
+            output[j] += hidden[i] * w2[i][j];
+        }
 
-}
+    }
     LOOP2K:for (j=0; j<OUTPUT_CLASS; j++) {
         output[j] += b2[j];  // +b2[OUTPUT_CLASS]
         if (output[j] > temp) { // predict
--- dnn.h
+++ dnn.h
@@ -18,7 +18,7 @@ using namespace std;
 // Float is the default type. But we need to use fixed point here. I provided two methods to produce fixed point:
 typedef ap_fixed<10,5> FIXED;       // Use library <ap_fixed.h>
 
-//typedef short FIXED;              // Self define.
+//typedef short FIXED;              // My define.
 //#define ftofx(f_x) (short)((f_x) * 256) // Float to fixed. If we need to reduce latency, reduce N in 2^N (now it's 2^8=256).
 //#define mulfx(fx_x, fx_y) (((fx_x) * (fx_y)) >> 8) // Fixed times fixed equals fixed. If we need to reduce latency, reduce N (now it's 8). 
 
